1cbd52b0f157c8162533931c80c8f44fd6d89f68
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-17 11:36:22 +0000

    monday

--- application/routes.py
+++ application/routes.py
@@ -78,7 +78,7 @@ def alterego():
   return render_template("searchalterego.html", search=hero)
 @app.route('/search/power', methods=['GET','POST'])
 def power():
-   if hero.validate_on_submit():
+  if hero.validate_on_submit():
     results=superhero.query.filter(superhero.p1==search.power.data or superhero.p2==search.power.data or superhero.p3==search.power.data).all()
     return render_template("show.html", superherodata=results) 
   return render_template("searchpower.html", search=hero)
@@ -96,7 +96,7 @@ def ():
   return render_template("searchsidekick.html", search=hero)
 @app.route('/search/', methods=['GET','POST'])
 def ():
-   if hero.validate_on_submit():
+  if hero.validate_on_submit():
     results=superhero.query.filter(superhero.nemesis==search.nemesis.data).all()
     return render_template("show.html", superherodata=results) 
   return render_template("searchnemesis.html", search=hero)
