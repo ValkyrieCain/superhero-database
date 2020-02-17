077c68031189bc009f8ffa449ae9320c44fbd026
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-17 11:59:03 +0000

    monday

--- application/routes.py
+++ application/routes.py
@@ -61,42 +61,49 @@ def all():
   return render_template("show.html", superherodata=results)
 @app.route('/search/publisher', methods=['GET','POST'])
 def publisher():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.publisher==search.publisher.data).all()
     return render_template("show.html", superherodata=results)
   return render_template("searchpublisher.html", search=hero)
 @app.route('/search/name', methods=['GET','POST'])
 def name():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.name==search.name.data).all()
     return render_template("show.html", superherodata=results)
   return render_template("searchname.html", search=hero)
 @app.route('/search/alterego', methods=['GET','POST'])
 def alterego():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.alterego==search.alterego.data).all()
     return render_template("show.html", superherodata=results)
   return render_template("searchalterego.html", search=hero)
 @app.route('/search/power', methods=['GET','POST'])
 def power():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.p1==search.power.data or superhero.p2==search.power.data or superhero.p3==search.power.data).all()
     return render_template("show.html", superherodata=results)
   return render_template("searchpower.html", search=hero)
 @app.route('/search/team', methods=['GET','POST'])
 def team():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.team==search.team.data).all()
     return render_template("show.html", superherodata=results)
   return render_template("searchteam.html", search=hero)
 @app.route('/search/sidekick', methods=['GET','POST'])
 def sidekick():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.sidekick==search.sidekick.data).all()
     return render_template("show.html", superherodata=results)
   return render_template("searchsidekick.html", search=hero)
 @app.route('/search/nemesis', methods=['GET','POST'])
 def nemesis():
+  hero=Hero()
   if hero.validate_on_submit():
     results=Superheroes.query.filter(superhero.nemesis==search.nemesis.data).all()
     return render_template("show.html", superherodata=results)
