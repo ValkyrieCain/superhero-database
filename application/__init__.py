d3f3f7f271359914ced2e3372253e9038df09b93
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-17 12:45:14 +0000

    monday

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from application import routes
 import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DB_URI'))
 app.config['SECRET_KEY'] = str(os.getenv('SK_NUM'))
 db = SQLAlchemy(app)
-from application import routes
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
--- application/forms.py
+++ application/forms.py
@@ -2,13 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import (IntegerField,StringField,SubmitField)
 from wtforms.validators import DataRequired, Length
 
-class FF(FlaskForm):
-	id=IntegerField(validators=[DataRequired()])
-	name=StringField()
-	submit=SubmitField()
-class Sch(FlaskForm):
-	search=StringField()
-	submitsearch=SubmitField()
 class Hero(FlaskForm):
 	publisher=StringField(validators=[DataRequired()])
 	name=StringField(validators=[DataRequired()])
