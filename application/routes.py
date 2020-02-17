ce838eed36d7b3439cebe62d070d0501b929c8ca
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-17 15:27:03 +0000

    monday

--- application/routes.py
+++ application/routes.py
@@ -36,7 +36,7 @@ def login():
   if current_user.is_authenticated:
     return redirect(url_for('home'))
   if form.validate_on_submit():
-    user=Users.query.filter(username==form.username.data).first()
+    user=Users.query.filter(Users.username==form.username.data).first()
     if user and bcrypt.check_password_hash(user.password, form.password.data):
       login_user(user, remember=form.remember.data)
       next_page = request.args.get('next')
