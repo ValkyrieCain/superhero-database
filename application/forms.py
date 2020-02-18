fd58b5be149d28d405dfcfe234918254fbd357b5
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-18 11:20:09 +0000

    tuesday

--- application/forms.py
+++ application/forms.py
@@ -28,7 +28,7 @@ class Search(FlaskForm):
 		if not result:
 			raise ValidationError('Hero does not exist')
 class Delete(FlaskForm):
-	confirmdelete=SubmitField('Yes',validators=validators.DataRequired())
+	confirmdelete=SubmitField('Yes',validators=[DataRequired()])
 	confirm=BooleanField('Delete hero?')
 class Register(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(),Length(max=30)])
