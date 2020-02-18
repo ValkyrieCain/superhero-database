a5b3e6eff0b58201117b9d1bf634d5c4d9f947fa
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-18 15:12:34 +0000

    tuesday

--- application/routes.py
+++ application/routes.py
@@ -111,7 +111,6 @@ def delete():
     print("search function")
     if update.validate_on_submit():
       print("delete function")
-    else:
     return render_template('update.html', data=result, hero=update)
   return render_template("searchalterego.html", search=search)
 @app.route('/saved')
