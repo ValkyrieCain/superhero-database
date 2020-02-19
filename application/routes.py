bdb27e0de9ff8cab197eda56adafcaa57ea3048d
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-19 15:19:02 +0000

    wednesday

--- application/routes.py
+++ application/routes.py
@@ -7,7 +7,7 @@ from application.forms import Hero, Search, Register, Login, Delete, Alterego
 import time
 def show(x):
   final={}
-  final.naaa=x[0]
+  final[naaa]=x[0]
   return final
 @app.route('/')
 @app.route('/home')
