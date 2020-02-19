a0d06b58301900f927e212dd245a1d39cc3b4e19
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-19 14:24:44 +0000

    wednesday

--- application/routes.py
+++ application/routes.py
@@ -181,12 +181,14 @@ def all():
 @app.route('/search/publisher', methods=['GET','POST'])
 def publisher():
   search=Search()
+  print(55)
   if search.validate_on_submit():
+    print(55)
     global pub
     pub=Superheroes.query.filter(Superheroes.publisher==search.publisher.data.upper()).all()
     print(pub)
     print("xxxxx")
-    return redirect("/search/results")
+    return redirect("/saved")
   return render_template("searchpublisher.html", search=search)
 @app.route('/search/name', methods=['GET','POST'])
 def name():
