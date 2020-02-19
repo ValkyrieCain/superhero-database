787a03629f017e05f147d1a7c7d53bdfb25f71f6
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-19 16:56:44 +0000

    wednesday

--- application/routes.py
+++ application/routes.py
@@ -144,7 +144,7 @@ def publisher():
   search=Search()
   if search.validate_on_submit():
     results=Superheroes.query.filter(Superheroes.publisher==search.publisher.data.upper()).all()
-    print(results.publisher)
+    print(results.publisher[0])
     #powers=[{"p1":"","p2":"","p3":""}]
     #for x in results:
     #  powers.p1=Powers.query.filter(Powers.id==x.p1)
