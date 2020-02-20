d4a3ad93cab34705893a3d3d571695678bc3bf8c
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-20 09:16:25 +0000

    thursday

--- application/routes.py
+++ application/routes.py
@@ -147,7 +147,7 @@ def publisher():
     #print(results.publisher.data)
     print(111111111111111)
     for x in results:
-      print(x.__dict__)
+      print(x.__dict__.name)
     print(111111111111111)
     return redirect(url_for('saved'))
     #powers=[{"p1":"","p2":"","p3":""}]
