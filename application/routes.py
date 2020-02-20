3ffd29a9ac5c697b17dde4572a0c2a05c9c125cc
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-20 16:37:25 +0000

    thursday

--- application/routes.py
+++ application/routes.py
@@ -245,7 +245,7 @@ def power():
     #pquery=Powers.query.filter(Powers.power.in_(search.power.data.upper())).first()
     pquery=Powers.query.filter(Powers.power==search.power.data.upper()).first()
     xx=Superheroes.query.filter(Superheroes.p1==pquery.id).all()
-    results=Superheroes.query.filter(or_(Superheroes.p1==pquery.id or Superheroes.p2==pquery.id or Superheroes.p3==pquery.id)).all()
+    results=Superheroes.query.filter(Superheroes.p1==pquery.id, Superheroes.p2==pquery.id, Superheroes.p3==pquery.id).all()
     p1=0
     p2=0
     p3=0
