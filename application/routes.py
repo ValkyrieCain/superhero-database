62efd52d33b315dcf8535a131f70af3bbf01588d
ValkyrieCain <valkyrie.cain@qa.com>
2020-02-21 13:43:17 +0000

    friday

--- application/routes.py
+++ application/routes.py
@@ -44,7 +44,7 @@ def logout():
 def create():
 #<form method='POST' action=''>
 #  {{alterego.hidden_tag()}}
-#  {{alterego.submit}}<br>ddsdadfsddsd
+#  {{alterego.submit}}<br>ddsdadfsdasdsadasdsaddsd
 #</form>
 #if alterego.validate_on_submit():
   hero=Hero()
